888be082f27bfedc7ea272b7f107186430aff737
kadduni <ynnyself19@gmail.com>
2026-07-14 01:51:31 +0800

    Add difficulty feature

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// game mode
+// ⏔⏔⏔ ꒰ game mode ꒱ ⏔⏔⏔
 const computerMode = document.getElementById('computer-play');
 const friendMode = document.getElementById('friend-play');
 
-// html elements
+// ⏔⏔⏔ ꒰ html elements ꒱ ⏔⏔⏔
 const startPanel = document.getElementById('start-panel');
 const difficultyPanel = document.getElementById('difficulty-panel');
 const gamePanel = document.getElementById('game-panel');
 
-// selecting difficulty (computer mode only)
-const easyDiff = document.getElementById('easy');
-const mediumDiff = document.getElementById('medium');
-const hardDiff = document.getElementById('hard');
-
-// back button
+// ⏔⏔⏔ ꒰ back button ꒱ ⏔⏔⏔
 const backButton = document.getElementById('exit');
 
 backButton.addEventListener('click', () => {
@@ -20,43 +15,28 @@ backButton.addEventListener('click', () => {
   startPanel.classList.remove('hide');
 });
 
+// ⏔⏔⏔ ꒰  computer mode ꒱ ⏔⏔⏔
 computerMode.addEventListener('click', () => {
-  showDifficultyPanel();
+  startPanel.classList.add('hide');
+  difficultyPanel.classList.remove('hide');
 });
 
-// computer variables
-let difficultyLevel = '';
+// selecting difficulty
+let difficultyLevel = 1;
+
+const easyDiff = document.getElementById('easy');
+const mediumDiff = document.getElementById('medium');
+const hardDiff = document.getElementById('hard');
 
 document.querySelectorAll('.difficulty')
   .forEach((difficultyButton) => {
     difficultyButton.addEventListener('click', () => {
+      // hide difficulty panel and show game panel
       difficultyPanel.classList.add('hide');
       gamePanel.classList.remove('hide');
       
       difficultyLevel = difficultyButton.dataset.difficultyId;
-      
-      playComputerMode(difficulty);
+
+      playComputerMode();
     });
   });
-
-friendMode.addEventListener('click', () => {
-  playFriendMode();
-});
-
-function showDifficultyPanel() {
-  startPanel.classList.add('hide');
-  difficultyPanel.classList.remove('hide');
-}
-
-// computer mode
-function playComputerMode(difficulty) {
-  showDifficultyPanel.classList.add('hide');
-  gamePanel.classList.remove('hide');
-
-  document.querySelectorAll('.cell')
-    .forEach((box) => {
-      box.addEventListener('click', () => {
-        const boxClicked = box.dataset.box;
-      });
-    });
-}
